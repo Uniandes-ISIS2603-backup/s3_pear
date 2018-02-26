b3bc9911b744380a4f68d3e1d708b6349288c1a7
JUAN SEBASTIAN CABRA LEON <js.cabra@sis.virtual.uniandes.edu.co>
2018-02-26 10:52:04 -0500

    logics

---
+++ s3_pear-back/src/main/java/co/edu/uniandes/csw/pear/ejb/ComidaLogic.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.pear.ejb;
+
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+
+/**
+ * Clase que implementa la conexion con la persistencia para la entidad de Comida.
+ * @author js.cabra
+ */
+@Stateless
+public class ComidaLogic {
+    
+    /**
+     * Constante que representa el logger
+     */
+    private static final Logger LOGGER = Logger.getLogger(ComidaLogic.class.getName());
+    
+    /**
+     * Conexion con Persistencia
+     */
+    private ComidaPersistance persistence;
+    
+    
+    /**
+     * METODOS 
+     */
+    
+    
+    
+    /**
+     * Retorna todas las Comidas Entities que se encuentran en la base de datos
+     * @return 
+     */
+    public List<ComidaEntity> getComidas() {
+        LOGGER.info("Inicia consulta de todas las comidas");
+        List<ComidaEntity> comidas =  persistence.findAll();
+        LOGGER.info("Termina la consulta de todas las comidas");
+        return comidas;
+    }
+    
+    /**
+     * Retorna una Comida Entity por id
+     * @param id
+     * @return 
+     */
+    public ComidaEntity getComida( Long id ) {
+        LOGGER.log(Level.INFO, "Inicia consulta de comida con id = {0}", id);
+        ComidaEntity comida = persistence.find(id);
+        if ( comida ==  null )
+            LOGGER.log(Level.INFO, "No existe una Comida con el id = {0}", id);
+        LOGGER.log(Level.INFO, "Termina la consulta de comida con id = {0}", id);
+        return comida;
+    }
+    
+    /**
+     * Crea una Comida y la guarda en la base de datos
+     * @param entity de comida a persistir
+     * @return entidad de comida persistida
+     */
+    public ComidaEntity createComida( ComidaEntity entity ) {
+        LOGGER.log(Level.INFO, "Inicia proceso de creacion de una comida con id = {0}", entity.getId());
+        persistence.create(entity);
+        LOGGER.log(Level.INFO, "Termina proceso de creacion de una comida con id = {0}", entity.getId());
+        return entity;
+    } 
+    
+    /**
+     * Actualiza una Comida por id
+     * @param id de tipo Long, representa la comida que se va a actualizar
+     * @param entity de Comida con los cambios deseados
+     * @return la entidad de Comida luego de ser actualizada
+     */
+    public ComidaEntity updateComida( Long id, ComidaEntity entity ) {
+        LOGGER.log(Level.INFO, "Inica proceso de actualizacion de la comida con id = {0} " , id);
+        ComidaEntity actualizado = persistence.update(entity);
+        LOGGER.log( Level.INFO, "Termina proceso de actualizacion de la comida, id = {0}", entity.getId() );
+        return actualizado;
+    }
+    
+    /**
+     * Elimina una Comida por id
+     * @param id 
+     */
+    public void delete( Long id ) {
+        LOGGER.log(Level.INFO, "Inicia eliminacion de la comida con id = {0} " , id);
+        persistence.delete(id);
+        LOGGER.log( Level.INFO, "Comida con id = {0} eliminada. ", id );
+    }
+    
+}
---
+++ s3_pear-back/src/main/java/co/edu/uniandes/csw/pear/ejb/EnvioLogic.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.pear.ejb;
+
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+
+/**
+ * Clase que implementa la conexion con la persistencia para la entidad de Envio.
+ * @author js.cabra
+ */
+@Stateless
+public class EnvioLogic {
+    
+    /**
+     * Constante que representa el logger
+     */
+    private static final Logger LOGGER = Logger.getLogger(EnvioLogic.class.getName());
+    
+    /**
+     * Conexion con Persistencia
+     */
+    private EnvioPersistance persistence;
+    
+    
+    /**
+     * METODOS 
+     */
+    
+    
+    
+    /**
+     * Retorna todas los Envios Entities que se encuentran en la base de datos
+     * @return 
+     */
+    public List<EnvioEntity> getEnvios() {
+        LOGGER.info("Inicia consulta de todos los envios");
+        List<EnvioEntity> envios =  persistence.findAll();
+        LOGGER.info("Termina la consulta de todos los envios");
+        return envios;
+    }
+    
+    /**
+     * Retorna un Envio Entity por id
+     * @param id
+     * @return 
+     */
+    public EnvioEntity getEnvio( Long id ) {
+        LOGGER.log(Level.INFO, "Inicia consulta de envios con id = {0}", id);
+        EnvioEntity envio = persistence.find(id);
+        if ( envio ==  null )
+            LOGGER.log(Level.INFO, "No existe un Envio con el id = {0}", id);
+        LOGGER.log(Level.INFO, "Termina la consulta de envio con id = {0}", id);
+        return envio;
+    }
+    
+    /**
+     * Crea un Envio y la guarda en la base de datos
+     * @param entity de envio a persistir
+     * @return entidad de envio persistida
+     */
+    public EnvioEntity createComida( EnvioEntity entity ) {
+        LOGGER.log(Level.INFO, "Inicia proceso de creacion de un envio con id = {0}", entity.getId());
+        persistence.create(entity);
+        LOGGER.log(Level.INFO, "Termina proceso de creacion de un envio con id = {0}", entity.getId());
+        return entity;
+    } 
+    
+    /**
+     * Actualiza un ENvio por id
+     * @param id de tipo Long, representa el envio que se va a actualizar
+     * @param entity de Envio con los cambios deseados
+     * @return la entidad de Envio luego de ser actualizada
+     */
+    public EnvioEntity updateComida( Long id, EnvioEntity entity ) {
+        LOGGER.log(Level.INFO, "Inica proceso de actualizacion de un envio con id = {0} " , id);
+        EnvioEntity actualizado = persistence.update(entity);
+        LOGGER.log( Level.INFO, "Termina proceso de actualizacion de un envio, id = {0}", entity.getId() );
+        return actualizado;
+    }
+    
+    /**
+     * Elimina un Envio por id
+     * @param id 
+     */
+    public void delete( Long id ) {
+        LOGGER.log(Level.INFO, "Inicia eliminacion de el envio con id = {0} " , id);
+        persistence.delete(id);
+        LOGGER.log( Level.INFO, "Envio con id = {0} eliminada. ", id );
+    }
+    
+}
