103b7baffaaab8412b1d21b31bc3c548c17f232b
v.chacon <v.chacon@ISIS2603S3-0076.sis.virtual.uniandes.edu.co>
2018-03-02 00:38:36 -0500

    Logic test progress

--- s3_pear-back/src/test/java/co/edu/uniandes/csw/pear/test/logic/FacturaLogicTest.java
+++ s3_pear-back/src/test/java/co/edu/uniandes/csw/pear/test/logic/FacturaLogicTest.java
@@ -5,10 +5,92 @@
  */
 package co.edu.uniandes.csw.pear.test.logic;
 
+import co.edu.uniandes.csw.pear.ejb.FacturaLogic;
+import co.edu.uniandes.csw.pear.entities.FacturaEntity;
+import co.edu.uniandes.csw.pear.persistence.FacturaPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 /**
  *
  * @author v.chacon
  */
+
+@RunWith(Arquillian.class)
 public class FacturaLogicTest {
+    
+    private PodamFactory factory = new PodamFactoryImpl();
+      
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    private UserTransaction utx;
+    
+    private List<FacturaEntity> data = new ArrayList<FacturaEntity>();
+    
+    @Inject
+    private FacturaLogic logic;
+    
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(FacturaEntity.class.getPackage())
+                .addPackage(FacturaLogic.class.getPackage())
+                .addPackage(FacturaPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from FacturaoEntity").executeUpdate();
+    }
+    
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            FacturaEntity entity = factory.manufacturePojo(FacturaEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
     
 }
--- s3_pear-back/src/test/java/co/edu/uniandes/csw/pear/test/logic/SemanaLogicTest.java
+++ s3_pear-back/src/test/java/co/edu/uniandes/csw/pear/test/logic/SemanaLogicTest.java
@@ -5,10 +5,91 @@
  */
 package co.edu.uniandes.csw.pear.test.logic;
 
+import co.edu.uniandes.csw.pear.ejb.SemanaLogic;
+import co.edu.uniandes.csw.pear.entities.SemanaEntity;
+import co.edu.uniandes.csw.pear.persistence.SemanaPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 /**
  *
  * @author v.chacon
  */
+@RunWith(Arquillian.class)
 public class SemanaLogicTest {
+    
+    private PodamFactory factory = new PodamFactoryImpl();
+      
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Inject
+    private UserTransaction utx;
+    
+    private List<SemanaEntity> data = new ArrayList<SemanaEntity>();
+    
+    @Inject
+    private SemanaLogic logic;
+    
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(SemanaEntity.class.getPackage())
+                .addPackage(SemanaLogic.class.getPackage())
+                .addPackage(SemanaPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    /**
+     * Configuración inicial de la prueba.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+       /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from SemanaEntity").executeUpdate();
+    }
+    
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        for (int i = 0; i < 3; i++) {
+            SemanaEntity entity = factory.manufacturePojo(SemanaEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
     
 }
