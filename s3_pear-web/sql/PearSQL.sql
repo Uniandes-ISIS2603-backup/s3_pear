33b524553f573c3351e88bdf5d50094478270f3f
pa-suarezm <pa.suarezm@uniandes.edu.co>
2018-03-05 11:11:56 -0500

    PearSQL

--- s3_pear-web/sql/PearSQL.sql
+++ s3_pear-web/sql/PearSQL.sql
@@ -1,5 +1,6 @@
 delete from CalificacionEntity;
 delete from QuejasyReclamosEntity;
+delete from CasoExitoEntity;
 
 insert into CalificacionEntity (id, puntuacion) values (1, 7);
 insert into CalificacionEntity (id, puntuacion) values (2, 1);
@@ -15,3 +16,16 @@ insert into QuejasyReclamosEntity (id, asunto, comentario) values (1,'queja', 'L
 insert into QuejasyReclamosEntity (id, asunto, comentario) values (2,'recomendacion', 'Llamar al cliente cuando el envio este cerca');
 insert into QuejasyReclamosEntity (id, asunto, comentario) values (3,'recomendacion', 'Contratar domiciliarios más amables');
 insert into QuejasyReclamosEntity (id, asunto, comentario) values (4,'queja', 'el domiciliario fue grosero');
+
+
+
+insert into CasoExitoEntity (id, comentario, testimonio) values (1, 'Comentario 1', 'Testimonio 1');
+insert into CasoExitoEntity (id, comentario, testimonio) values (2, 'Comentario 2', 'Testimonio 2');
+insert into CasoExitoEntity (id, comentario, testimonio) values (3, 'Comentario 3', 'Testimonio 3');
+insert into CasoExitoEntity (id, comentario, testimonio) values (4, 'Comentario 4', 'Testimonio 4');
+insert into CasoExitoEntity (id, comentario, testimonio) values (5, 'Comentario 5', 'Testimonio 5');
+insert into CasoExitoEntity (id, comentario, testimonio) values (6, 'Comentario 6', 'Testimonio 6');
+insert into CasoExitoEntity (id, comentario, testimonio) values (7, 'Comentario 7', 'Testimonio 7');
+insert into CasoExitoEntity (id, comentario, testimonio) values (8, 'Comentario 8', 'Testimonio 8');
+insert into CasoExitoEntity (id, comentario, testimonio) values (9, 'Comentario 9', 'Testimonio 9');
+insert into CasoExitoEntity (id, comentario, testimonio) values (10, 'Comentario 10', 'Testimonio 10');
