a8016ef7e861c535495cc04bf3ccbaf62a4f7dba
Sebastian Garcia <sebastiangarcia@Sebastians-MacBook-Pro.local>
2018-04-20 00:34:45 -0500

    post de dieta y cocina, detail de dieta

--- s3_pear-web/src/main/webapp/src/modules/cocinas/cocinas.mod.js
+++ s3_pear-web/src/main/webapp/src/modules/cocinas/cocinas.mod.js
@@ -1,33 +1,49 @@
 (function (ng) {
 
-	var mod = ng.module("cocinaModule", ['ui.router']);
+    var mod = ng.module("cocinaModule", ['ui.router']);
 
-	mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
-		$urlRouterProvider.otherwise("/dietas");
+        $urlRouterProvider.otherwise("/dietas");
 
-		$stateProvider
-			.state('cocinas', {
-				url: "/cocinas",
-				templateUrl: "src/modules/cocinas/cocina_list.html",
-				controller: 'cocinasController'
-			})  ;
+        $stateProvider
+            .state('cocinas', {
+                url: "/cocinas",
+                templateUrl: "src/modules/cocinas/cocina_list.html",
+                controller: 'cocinasController'
+            });
     }]);
 
 })(window.angular);
 
 (function (ng) {
 
-	var mod = ng.module("cocinaModule");
-	mod.constant("cocinasContext", "api/cocinas");
+    var mod = ng.module("cocinaModule");
+    mod.constant("cocinasContext", "api/cocinas");
 
-	mod.controller('cocinasController', ['$scope', '$http', 'cocinasContext',
+    mod.controller('cocinasController', ['$scope', '$http', 'cocinasContext',
 
         function ($scope, $http, cocinasContext) {
-			//http://localhost:8080/s3_pear-web/api/cocinas
-			$http.get('src/modules/cocinas/cocinas.json').then(function (response) {
-				$scope.cocinas = response.data;
-			});
+            //http://localhost:8080/s3_pear-web/api/cocinas
+            $http.get('src/modules/cocinas/cocinas.json').then(function (response) {
+                $scope.cocinas = response.data;
+            });
+            
+            
+            $scope.enviar_cocina = function() {
+                let data = {
+                    capacidad: $scope.capacidad,
+                    direccion: $scope.direccion
+                };
+                
+                console.log(data);
+
+                // DIRECCION HTTP 
+                /*$http.post(' DIRECCION HTTP DE POSTMAN', data).then(function (response) {
+                    $scope.post_data = response.data;
+                });*/
+            }
+            
         }
 
     ]);
