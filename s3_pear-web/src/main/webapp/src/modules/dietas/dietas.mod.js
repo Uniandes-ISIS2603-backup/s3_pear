6861112ebf5c9ed0308753e29d64e480efe65a8a
Sebastian Garcia <sebastiangarcia@Sebastians-MacBook-Pro-3.local>
2018-04-20 21:44:20 -0500

    cambio en json de dietas y coinas

--- s3_pear-web/src/main/webapp/src/modules/dietas/dietas.mod.js
+++ s3_pear-web/src/main/webapp/src/modules/dietas/dietas.mod.js
@@ -1,27 +1,27 @@
 (function (ng) {
 
-	var mod = ng.module("dietaModule", ['ui.router']);
+    var mod = ng.module("dietaModule", ['ui.router']);
 
-	mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
-		$urlRouterProvider.otherwise("/dietas");
+        $urlRouterProvider.otherwise("/dietas");
 
-		$stateProvider
-			.state('dietas', {
-				url: "/dietas",
-				templateUrl: "src/modules/dietas/dietas.html",
-				controller: 'dietasController'
-			})  
-        
+        $stateProvider
+            .state('dietas', {
+                url: "/dietas",
+                templateUrl: "src/modules/dietas/dietas.html",
+                controller: 'dietasController'
+            })
+
             .state('dietas.detail', {
-				url: "/dieta/:id/detail",
+                url: "/dieta/:id/detail",
                 param: {
                     id: null
                 },
-				templateUrl: "src/modules/dietas/dieta_detail.html",
-				controller: 'dietasController'
-			}) 
-        
+                templateUrl: "src/modules/dietas/dieta_detail.html",
+                controller: 'dietasController'
+            })
+
         ;
     }]);
 
@@ -29,50 +29,53 @@
 
 (function (ng) {
 
-	var mod = ng.module("dietaModule");
-	mod.constant("dietasContext", "api/dietas");
+    var mod = ng.module("dietaModule");
+    mod.constant("dietasContext", "api/dietas");
 
-	mod.controller('dietasController', ['$scope', '$http', 'dietasContext',
+    mod.controller('dietasController', ['$scope', '$http', 'dietasContext',
 
         function ($scope, $http, dietasContext) {
-			//http://localhost:8080/s3_pear-web/api/dietas
-			$http.get('src/modules/dietas/dietas.json').then(function (response) {
-				$scope.dietas = response.data;
-			}).then(function (response) {
+            //http://localhost:8080/s3_pear-web/api/dietas
+
+            $http.get('src/modules/dietas/dietas.json').then(function (response) {
+                $scope.dietas = response.data;
+            }).then(function (response) {
                 console.log('FAILED GET DIETAS ' + response);
-            })
-            ;
+            });
             
+
             $scope.enviar_dieta = function () {
-            
+
                 let data = {
-                    
+
                     //Aqui hay que poner el identificador??
                     nombre: $scope.nombre,
                     descripcion: $scope.descripcion,
                     objetivo: $scope.objetivos
                 };
-                
+
                 console.log(data);
 
                 // DIRECCION HTTP 
                 /*$http.post(' DIRECCION HTTP DE POSTMAN', data).then(function (response) {
                     $scope.post_data = response.data;
                 });*/
-            }
-            
-            
-            $scope.eliminar_dieta = function ( id ) {
-            
-                console.log(id + " < Se va a eliminar la dieta" );
+            };
+
+
+            $scope.eliminar_dieta = function (id) {
+
+                console.log(id + " < Se va a eliminar la dieta");
 
                 // DIRECCION HTTP 
                 /*$http.delete(' DIRECCION HTTP DE POSTMAN' + id ).then(function (response) {
                     $scope.post_data = response.data;
                 });*/
-            }
+            };
             
             
+
+
         }
 
     ]);
