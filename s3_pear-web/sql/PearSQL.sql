bb6b4a545b2b774b60d02e1d748874989cb30ca3
JUAN SEBASTIAN GARCIA LOPEZ <js.garcial1@sis.virtual.uniandes.edu.co>
2018-04-28 09:47:24 -0500

    sql

--- s3_pear-web/sql/PearSQL.sql
+++ s3_pear-web/sql/PearSQL.sql
@@ -1,29 +1,102 @@
-delete from CalificacionEntity;
-delete from QuejasyReclamosEntity;
-delete from CasoExitoEntity;
-delete from PersonaEntity;
+
+UPDATE DIETATIPOENTITY SET CUENTACOBRO_ID = NULL, COCINA_ID = NULL;
+
+UPDATE CUENTACOBROENTITY SET DIETA_ID = NULL;
+UPDATE FACTURAENTITY SET PERSONA_ID = NULL;
+
+delete from CuentaCobroEntity;
 delete from DietaTipoEntity;
+
+
 delete from CocinaEntity;
+
 delete from ComidaEntity;
 delete from EnvioEntity;
 delete from DiaEntity;
 delete from SemanaEntity;
-delete from FacturaEntity;
+
 delete from MedioPagoEntity; 
 delete from PagoEntity; 
-delete from CuentaCobroEntity; 
+ 
+
+delete from PersonaEntity;
+delete from FacturaEntity;
+
+
+delete from CalificacionEntity;
+delete from QuejasyReclamosEntity;
+delete from CasoExitoEntity;
 
 
 
 
 
-INSERT INTO DietaTipoEntity(descripcion,id,objetivo,stars,imagen,name) VALUES ('Enterprise-wide coherent access',1,'Versatile tangible task-force',1,'https://images.pexels.com/photos/248444/pexels-photo-248444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260','Dieta Japonesa');
 
-insert into CuentaCobroEntity(id, valorAPagar, factura_id, dieta_id ) values (1, 20, 45, 1);
-insert into CuentaCobroEntity(id, valorAPagar, factura_id, dieta_id ) values (1, 20, 46, 1);
+insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (1, 'Pablo', 'Suarez', 20, 'Cll. 127D #19-93', 'pa.suarezm@uniandes.edu.co', 0, '201632293');
+insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (2, 'Camilo', 'Bedoya', 20, 'Cll. 151 #9-26', 'jc.bedoya@uniandes.edu.co', 0, '201631591');
+insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (3, 'Carol', 'Trujillo', 19, 'Cra. 53 #127-43', 'cv.trujillo@uniandes.edu.co', 0, '201618005');
+insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (4, 'Mariana', 'Moreno', 20, 'Cll. 76 #7-83', 'mi.morenoa@uniandes.edu.co', 0, '201698416');
+insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (5, 'Juan', 'Suarez', 23, 'Cll. 127D #19-93', 'jm.suarezm@uniandes.edu.co', 0, '201632294');
+insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (6, 'Elsa', 'Murillo', 54, 'Cll. 127D #19-93', 'ev.murilloa@uniandes.edu.co', 0, '201632295');
+insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (7, 'Antonio', 'Suarez', 55, 'Cll. 127D #19-93', 'aj.suarezb@uniandes.edu.co', 0, '201632296');
+insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (8, 'Gustavo', 'Quijano', 63, 'Cra. 7 #116-56', 'g.quijano@uniandes.edu.co', 0, '201632297');
+insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (9, 'Ana', 'Suarez', 65, 'Cll. 151 #9-66', 'ae.suarezb@uniamdes.edu.co', 0, '201632298');
+insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (10, 'Luisa', 'Estrada', 20, 'Cll. 131 #19-86', 'l.estradap@uniandes.edu.co', 0, '201712345');
+
+
+
+insert into FacturaEntity(id, cantidadProductos, numeroFacturaDeVenta, persona_id) values(45, 3, 200418,1);
+insert into FacturaEntity(id, cantidadProductos, numeroFacturaDeVenta, persona_id) values(46, 7, 300418,2);
+insert into FacturaEntity(id, cantidadProductos, numeroFacturaDeVenta, persona_id) values(47, 8, 400418,3);
+insert into FacturaEntity(id, cantidadProductos, numeroFacturaDeVenta, persona_id) values(48, 2, 100518,4);
+insert into FacturaEntity(id, cantidadProductos, numeroFacturaDeVenta, persona_id) values(49, 1, 200518,5);
 
 
 insert into PagoEntity(id, montoInicial, montoFinal) values (2, 10, 10);
+
+
+insert into CuentaCobroEntity(id, valorAPagar, factura_id ) values (1, 20, 45);
+insert into CuentaCobroEntity(id, valorAPagar, factura_id ) values (2, 20, 46);
+
+
+
+
+INSERT INTO COCINAENTITY (CAPACIDAD, IMAGEN, "NAME", UBICACION) 
+	VALUES ('2008', 'https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'La Gran Cocina', '8871 Merry Point');
+INSERT INTO COCINAENTITY (CAPACIDAD, IMAGEN, "NAME", UBICACION) 
+	VALUES ('232', 'https://images.pexels.com/photos/159991/pexels-photo-159991.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Cocina de las Aguas', '7303 Troy Placet');
+INSERT INTO COCINAENTITY (CAPACIDAD, IMAGEN, "NAME", UBICACION) 
+	VALUES ('2308', 'https://images.pexels.com/photos/225448/pexels-photo-225448.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Central del Norte', '652 Lukken Junction');
+INSERT INTO COCINAENTITY (CAPACIDAD, IMAGEN, "NAME", UBICACION) 
+	VALUES ('208', 'https://images.pexels.com/photos/2755/restaurant.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Cocina Bogota', '15 Hoepker Plaza');
+
+
+INSERT INTO DIETATIPOENTITY (DESCRIPCION, IMAGEN, "NAME", OBJETIVO, STARS, COCINA_ID) 
+	VALUES ('Vision-oriented intermediate matrix', 'https://images.pexels.com/photos/628777/pexels-photo-628777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Dieta Verde', 'Ergonomic solution-oriented support', 2, NULL);
+INSERT INTO DIETATIPOENTITY (DESCRIPCION, IMAGEN, "NAME", OBJETIVO, STARS, COCINA_ID) 
+	VALUES ('Enterprise-wide coherent access', 'https://images.pexels.com/photos/248444/pexels-photo-248444.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Dieta Japonesa', 'Versatile tangible task-force', 1, NULL);
+INSERT INTO DIETATIPOENTITY (DESCRIPCION, IMAGEN, "NAME", OBJETIVO, STARS, COCINA_ID) 
+	VALUES ('Operative motivating strategy', 'https://images.pexels.com/photos/660282/pexels-photo-660282.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Anti Dieta', 'Balanced tertiary strategy', 5, NULL);
+INSERT INTO DIETATIPOENTITY (DESCRIPCION, IMAGEN, "NAME", OBJETIVO, STARS, COCINA_ID) 
+	VALUES ('Digitized real-time implementation', 'https://images.pexels.com/photos/628776/pexels-photo-628776.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Dieta Oriental', 'Enterprise-wide global architecture', 4, NULL);
+INSERT INTO DIETATIPOENTITY (DESCRIPCION, IMAGEN, "NAME", OBJETIVO, STARS, COCINA_ID) 
+	VALUES ('De-engineered multi-state info-mediaries', 'https://images.pexels.com/photos/546945/pexels-photo-546945.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'Dieta Italiana', 'Multi-tiered cohesive contingency', 4, NULL);
+INSERT INTO DIETATIPOENTITY (DESCRIPCION, IMAGEN, "NAME", OBJETIVO, STARS, COCINA_ID) 
+	VALUES ('ntegrated multimedia initiative', 'https://images.pexels.com/photos/629093/pexels-photo-629093.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Dieta de Crudos', 'Multi-layered explicit circu', 3, NULL);
+INSERT INTO DIETATIPOENTITY (DESCRIPCION, IMAGEN, "NAME", OBJETIVO, STARS, COCINA_ID) 
+	VALUES ('dieta basada en ensaladas o algo asi', 'https://images.pexels.com/photos/5938/food-salad-healthy-lunch.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Dieta Nutricional', 'el objetivo de la dieta es desintoxicarse', 3, NULL);
+INSERT INTO DIETATIPOENTITY (DESCRIPCION, IMAGEN, "NAME", OBJETIVO, STARS, COCINA_ID) 
+	VALUES ('una dieta basada en proteinas', 'https://images.pexels.com/photos/76093/pexels-photo-76093.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Dieta Deportiva', 'el objetivo de la dieta es obtener masa muscular', 5, NULL);
+INSERT INTO DIETATIPOENTITY (DESCRIPCION, IMAGEN, "NAME", OBJETIVO, STARS, COCINA_ID, ID ) 
+	VALUES ('dieta basada en alimentos del mar', 'https://images.pexels.com/photos/36768/food-salmon-teriyaki-cooking.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'Dieta Pacifica', 'el objetivo de la dieta es alejarse de las carnes rojas', 4, NULL, 1);
+
+
+/*
+UPDATE CUENTACOBROENTITY SET DIETA_ID = 1;
+*/
+
+
+
 
 insert into MedioPagoEntity(id, medioPagoActual) values (1, 'Baloto');
 insert into MedioPagoEntity(id, medioPagoActual) values (2, 'Efectivo');
@@ -31,26 +104,7 @@ insert into MedioPagoEntity(id, medioPagoActual) values (3, 'Credito');
 
 
 
-INSERT INTO DietaTipoEntity(descripcion,id,objetivo,stars,imagen,name) VALUES ('Vision-oriented intermediate matrix',2,'Ergonomic solution-oriented support',2,'https://images.pexels.com/photos/628777/pexels-photo-628777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260','Dieta Verde');
-INSERT INTO DietaTipoEntity(descripcion,id,objetivo,stars,imagen,name) VALUES ('Operative motivating strategy',3,'Balanced tertiary strategy',5,'https://images.pexels.com/photos/660282/pexels-photo-660282.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260','Anti Dieta');
-INSERT INTO DietaTipoEntity(descripcion,id,objetivo,stars,imagen,name) VALUES ('Digitized real-time implementation',4,'Enterprise-wide global architecture',4,'https://images.pexels.com/photos/628776/pexels-photo-628776.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260','Dieta Oriental');
-INSERT INTO DietaTipoEntity(descripcion,id,objetivo,stars,imagen,name) VALUES ('De-engineered multi-state info-mediaries',5,'Multi-tiered cohesive contingency',4,'https://images.pexels.com/photos/546945/pexels-photo-546945.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260','Dieta Italiana');
-INSERT INTO DietaTipoEntity(descripcion,id,objetivo,stars,imagen,name) VALUES ('ntegrated multimedia initiative',6,'Multi-layered explicit circu',3,'https://images.pexels.com/photos/629093/pexels-photo-629093.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940','Dieta de Crudos');
-INSERT INTO DietaTipoEntity(descripcion,id,objetivo,stars,imagen,name) VALUES ('dieta basada en ensaladas o al asi',9,'el objetivo de la dieta es desintoxicarse',3,'https://images.pexels.com/photos/5938/food-salad-healthy-lunch.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940','Dieta Nutricional');
-INSERT INTO DietaTipoEntity(descripcion,id,objetivo,stars,imagen,name) VALUES ('una dieta basada en proteinas',8,'el objetivo de la dieta es obtener masa muscular',5,'https://images.pexels.com/photos/76093/pexels-photo-76093.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940','Dieta Deportiva');
-INSERT INTO DietaTipoEntity(descripcion,id,objetivo,stars,imagen,name) VALUES ('dieta basada en alimentos del mar',7,'el objetivo de la dieta es alejarse de las carnes rojas',4,'https://images.pexels.com/photos/36768/food-salmon-teriyaki-cooking.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940','Dieta Pacifica');
 
-
-
-INSERT INTO CocinaEntity(capacidad,id,ubicacion,name,imagen) VALUES ('2008',1,'8871 Merry Point','La Gran Cocina','https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-/*INSERT INTO CocinaEntity(capacidad,id,ubicacion,name,imagen) VALUES ('232',2,'7303 Troy Placet','Cocina de las Aguas','https://images.pexels.com/photos/159991/pexels-photo-159991.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-INSERT INTO CocinaEntity(capacidad,id,ubicacion,name,imagen) VALUES ('2308',3,'652 Lukken Junction','Central del Norte','https://images.pexels.com/photos/225448/pexels-photo-225448.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-INSERT INTO CocinaEntity(capacidad,id,ubicacion,name,imagen) VALUES ('208',4,'15 Hoepker Plaza','Cocina Bogota','https://images.pexels.com/photos/2755/restaurant.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-/*INSERT INTO CocinaEntity(capacidad,id,ubicacion,name,imagen) VALUES ('28',5,'8871 Merry Point','Un Nombre de Cocina','https://images.pexels.com/photos/696205/pexels-photo-696205.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-INSERT INTO CocinaEntity(capacidad,id,ubicacion,name,imagen) VALUES ('2028',6,'29 Larry Parkway','Otro Nombre de Cocina','https://images.pexels.com/photos/375897/pexels-photo-375897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-INSERT INTO CocinaEntity(capacidad,id,ubicacion,name,imagen) VALUES ('38',7,'5073 Lakeland Lane','Cocina X','https://images.pexels.com/photos/64613/pexels-photo-64613.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-INSERT INTO CocinaEntity(capacidad,id,ubicacion,name,imagen) VALUES ('4008',8,'3 Meadow Ridge Plaza','SUper Cocina','https://images.pexels.com/photos/917371/pexels-photo-917371.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
-*/
 
 
 insert into CalificacionEntity (id, puntuacion) values (1, 7);
@@ -78,16 +132,8 @@ insert into CasoExitoEntity (id, comentario, testimonio) values (8, 'Comentario 
 insert into CasoExitoEntity (id, comentario, testimonio) values (9, 'Comentario 9', 'Testimonio 9');
 insert into CasoExitoEntity (id, comentario, testimonio) values (10, 'Comentario 10', 'Testimonio 10');
 
-insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (1, 'Pablo', 'Suarez', 20, 'Cll. 127D #19-93', 'pa.suarezm@uniandes.edu.co', 0, '201632293');
-insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (2, 'Camilo', 'Bedoya', 20, 'Cll. 151 #9-26', 'jc.bedoya@uniandes.edu.co', 0, '201631591');
-insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (3, 'Carol', 'Trujillo', 19, 'Cra. 53 #127-43', 'cv.trujillo@uniandes.edu.co', 0, '201618005');
-insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (4, 'Mariana', 'Moreno', 20, 'Cll. 76 #7-83', 'mi.morenoa@uniandes.edu.co', 0, '201698416');
-insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (5, 'Juan', 'Suarez', 23, 'Cll. 127D #19-93', 'jm.suarezm@uniandes.edu.co', 0, '201632294');
-insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (6, 'Elsa', 'Murillo', 54, 'Cll. 127D #19-93', 'ev.murilloa@uniandes.edu.co', 0, '201632295');
-insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (7, 'Antonio', 'Suarez', 55, 'Cll. 127D #19-93', 'aj.suarezb@uniandes.edu.co', 0, '201632296');
-insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (8, 'Gustavo', 'Quijano', 63, 'Cra. 7 #116-56', 'g.quijano@uniandes.edu.co', 0, '201632297');
-insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (9, 'Ana', 'Suarez', 65, 'Cll. 151 #9-66', 'ae.suarezb@uniamdes.edu.co', 0, '201632298');
-insert into PersonaEntity (id, nombre, apellido, edad, direccion, correo, subscrito, identificacion) values (10, 'Luisa', 'Estrada', 20, 'Cll. 131 #19-86', 'l.estradap@uniandes.edu.co', 0, '201712345');
+
+
 
 
 insert into ComidaEntity(id,cantidad,alimentos,TIPO) values (01,10,'ejemplo1','desayuno'/**,'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'*/);
@@ -135,11 +181,3 @@ insert into DiaEntity(id, recomendacion, fecha, seEnvia, semana_id) values (20, 
 insert into DiaEntity(id, recomendacion, fecha, seEnvia, semana_id) values (21, 'Una harina diaria','15/04/2018', 0,223);
 
  
-insert into FacturaEntity(id, cantidadProductos, numeroFacturaDeVenta, persona_id) values(45, 3, 200418,1);
-insert into FacturaEntity(id, cantidadProductos, numeroFacturaDeVenta, persona_id) values(46, 7, 300418,2);
-insert into FacturaEntity(id, cantidadProductos, numeroFacturaDeVenta, persona_id) values(47, 8, 400418,3);
-insert into FacturaEntity(id, cantidadProductos, numeroFacturaDeVenta, persona_id) values(48, 2, 100518,4);
-insert into FacturaEntity(id, cantidadProductos, numeroFacturaDeVenta, persona_id) values(49, 1, 200518,5);
-
-
-Select * from ComidaEntity;
