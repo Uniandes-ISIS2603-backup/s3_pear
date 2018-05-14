28d1ba7015264fbf6f0f9bbfead696b958b98645
jp.campos <jp.campos@ISIS2603S3-0070.sis.virtual.uniandes.edu.co>
2018-05-14 12:46:50 -0500

    Base entity .equals

--- s3_pear-back/src/main/java/co/edu/uniandes/csw/pear/entities/BaseEntity.java
+++ s3_pear-back/src/main/java/co/edu/uniandes/csw/pear/entities/BaseEntity.java
@@ -64,6 +64,16 @@ public abstract class BaseEntity implements Serializable {
 
     @Override
     public boolean equals(Object obj) {
+        
+        if(obj == null)
+        {
+            return false; 
+        }
+        if(this.getClass() != obj.getClass())
+        {
+            return false;
+        }
+        
         if (this.getId() != null && ((BaseEntity) obj).getId() != null) {
             return this.getId().equals(((BaseEntity) obj).getId());
         }
